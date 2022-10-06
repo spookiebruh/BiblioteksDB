bb2b9bc0934d9655a19f01807ee07b8ace96ce66
spookiebruh <zackeboibeat@gmail.com>
2022-10-06 15:38:26 +0200

    LÅNEFUNKTION

--- anvandare.php
+++ anvandare.php
@@ -1,5 +1,118 @@
 <?php
-  require_once("lana.php");
+  require_once("conn.php");
+  session_start();
+
+  // -------------------------------------------- //
+  // --------------- Bok ------------------------ //
+  // -------------------------------------------- //
+
+  //Defining variables
+  $db = $conn;
+  $tableNameBok="bok";
+  $columnsBok= ['Titel', 'Forfattare', 'Genre', 'Antalsidor'];
+  //Fetching data
+  $fetchDataBok = fetch_data($db, $tableNameBok, $columnsBok);
+  //Defining function "fetch_data"
+  function fetch_data($db, $tableNameBok, $columnsBok){
+    if(empty($db)){
+     $msg= "Database connection error";
+    }elseif (empty($columnsBok) || !is_array($columnsBok)) {
+     $msg="columns Name must be defined in an indexed array";
+    }elseif(empty($tableNameBok)){
+      $msg= "Table Name is empty";
+    }else{
+
+      $columnNameBok = implode(", ", $columnsBok);
+
+      $sql = "SELECT ".$columnNameBok." FROM $tableNameBok"." ORDER BY titel";
+      $result = $db->query($sql);
+  
+      if($result == true){ 
+        if ($result->num_rows > 0) {
+           $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+           $msg= $row;
+        } else {
+           $msg= "No Data Found"; 
+        }
+       }else{
+         $msg= mysqli_error($db);
+       }
+       }
+       return $msg;
+       }
+    // -------------------------------------------- //
+  // --------------- Ljud ----------------------- //
+  // -------------------------------------------- //
+
+  $tableNameLjud="ljud";
+  $columnsLjud= ['Titel', 'Forfattare', 'Genre', 'Langd', 'Rost'];
+  //Fetching data
+  $fetchDataLjud = fetch_data_ljud($db, $tableNameLjud, $columnsLjud);
+  //Defining function "fetch_data"
+  function fetch_data_ljud($db, $tableNameLjud, $columnsLjud){
+    if(empty($db)){
+     $msgLjud = "Database connection error";
+    }elseif (empty($columnsLjud) || !is_array($columnsLjud)) {
+     $msgLjud = "columns Name must be defined in an indexed array";
+    }elseif(empty($tableNameLjud)){
+      $msgLjud = "Table Name is empty";
+    }else{
+
+    $columnNameLjud = implode(", ", $columnsLjud);
+
+    $sqlLjud = "SELECT ".$columnNameLjud." FROM $tableNameLjud"." ORDER BY titel";
+    $resultLjud = $db->query($sqlLjud);
+
+    if($resultLjud == true){ 
+      if ($resultLjud->num_rows > 0) {
+         $rowLjud = mysqli_fetch_all($resultLjud, MYSQLI_ASSOC);
+         $msgLjud = $rowLjud;
+      } else {
+         $msgLjud = "No Data Found"; 
+      }
+     }else{
+       $msgLjud = mysqli_error($db);
+     }
+     }
+     return $msgLjud;
+     }
+
+  // -------------------------------------------- //
+  // --------------- Film ----------------------- //
+  // -------------------------------------------- //
+
+  $tableNameFilm="film";
+  $columnsFilm= ['Titel', 'Regissor', 'Genre', 'Langd'];
+  //Fetching data
+  $fetchDataFilm = fetch_data_film($db, $tableNameFilm, $columnsFilm);
+  //Defining function "fetch_data"
+  function fetch_data_film($db, $tableNameFilm, $columnsFilm){
+    if(empty($db)){
+     $msgFilm = "Database connection error";
+    }elseif (empty($columnsFilm) || !is_array($columnsFilm)) {
+     $msgFilm = "columns Name must be defined in an indexed array";
+    }elseif(empty($tableNameFilm)){
+      $msgFilm = "Table Name is empty";
+    }else{
+
+    $columnNameFilm = implode(", ", $columnsFilm);
+
+    $sqlFilm = "SELECT ".$columnNameFilm." FROM $tableNameFilm"." ORDER BY titel";
+    $resultFilm = $db->query($sqlFilm);
+
+    if($resultFilm == true){ 
+      if ($resultFilm->num_rows > 0) {
+         $rowFilm = mysqli_fetch_all($resultFilm, MYSQLI_ASSOC);
+         $msgFilm = $rowFilm;
+      } else {
+         $msgFilm = "No Data Found"; 
+      }
+     }else{
+       $msgFilm = mysqli_error($db);
+     }
+     }
+     return $msgFilm;
+     }
 ?>
 
 
@@ -23,6 +136,7 @@
           <table class="table table-bordered">
           <thead>
             <tr>
+            <th>ID</th>
             <th>Titel</th>
             <th>Författare</th>
             <th>Genre</th>
@@ -30,17 +144,26 @@
           </thead>
         <tbody>
       <?php
+
+
+          $db = $conn;
+          $tableNameBok="bok";
+          $columnsBok= ['BID', 'Titel', 'Forfattare', 'Genre', 'Antalsidor'];
+
+          $fetchDataBok = fetch_data($db, $tableNameBok, $columnsBok);
           if(is_array($fetchDataBok)){      
           $sn=1;
           foreach($fetchDataBok as $data){
         ?>
-          <tr>
-            <td><?php echo $data['Titel']??''; ?></td>
-            <td><?php echo $data['Forfattare']??''; ?></td>
-            <td><?php echo $data['Genre']??''; ?></td>
-            <td><?php echo $data['Antalsidor']??''; ?></td>
+          <form method="post" action="lana.php">
+            <input type="hidden" name="Bocker">
+            <td><?php echo $_SESSION["BID"] = $data['BID']??''; ?></td>
+            <td><?php echo $_SESSION["Titel"] = $data['Titel']??''; ?></td>
+            <td><?php echo $_SESSION["Forfattare"] = $data['Forfattare']??''; ?></td>
+            <td><?php echo $_SESSION["Genre"] = $data['Genre']??''; ?></td>
+            <td><?php echo $_SESSION["Antalsidor"] = $data['Antalsidor']??''; ?></td>
             <td><input type="submit" value="Låna"></td>
-          </tr>
+          </form>
     
         <?php }}?>
         </tbody>
@@ -123,6 +246,8 @@
     </div>
     </div>
   </div>
+
+
 </body>
 </html>
 
