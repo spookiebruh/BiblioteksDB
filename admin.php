89c88eb633e6858ad69ebab5283dbdc4a37e6535
Albin Andersson <albin.andersson6@elev.ga.ntig.se>
2022-10-06 09:15:48 +0200

    fungerande insert för bok ljud och film

--- admin.php
+++ admin.php
@@ -12,6 +12,7 @@
             <input type="hidden" name="Person">
             <input type="text" id="bok" name="Titel" max="100" placeholder="Namn" required="required">
             <input type="text" id="bok" name="forf"  placeholder="Password" required="required">
+            <input type="checkbox" id="bok" name="forf"  placeholder="Admin eller inte?" required="required">
             <input type="submit">
     </form> 
 
@@ -23,14 +24,14 @@
             <input type="text" id="bok" name="genre"  placeholder="Genre" required="required">
             <input type="number" id="bok" name="sidor"  placeholder="Antal sidor" required="required">
             <input type="number" id="bok" name="pid"  placeholder="Person ID" required="required">
-            <input type="number" id="bok" name="ref" Min="0" Max="1" placeholder="Referens" required="required">
+            <input type="checkbox" id="bok" name="ref" Min="0" Max="1" placeholder="Referens" required="required">
             <input type="submit">
         </form>  
         <form method="post" action="laggatill.php">  
             <input type="hidden" name="ljud">
-            <input type="text" id="Film" name="titel2" max="100" placeholder="Titel" required="required">
+            <input type="text" id="Film" name="titel2" max="100" placeholder="Lägga till ljudbok?" required="required">
             <input type="text" id="Film" name="forf2" max="100" placeholder="Författare" required="required">
-            <input type="number" id="Film" name="langd" max="100" placeholder="Langd" required="required">
+            <input type="number" id="Film" name="langd" max="100" placeholder="Längd" required="required">
             <input type="text" id="Film" name="genre2" max="100" placeholder="Genre" required="required">
             <input type="text" id="Film" name="rost" max="100" placeholder="Röst" required="required">
             <input type="number" id="Film" name="pid2" max="100" placeholder="Person ID" required="required">
@@ -38,9 +39,9 @@
         </form>  
         <form method="post" action="laggatill.php">  
             <input type="hidden" name="Film">
-            <input type="text" id="Film" name="titel3" max="100" placeholder="Titel" required="required">
+            <input type="text" id="Film" name="titel3" max="100" placeholder="Lägga till film?" required="required">
             <input type="text" id="Film" name="regi" max="100" placeholder="Regissör" required="required">
-            <input type="number" id="Film" name="langd2" max="100" placeholder="Langd" required="required">
+            <input type="number" id="Film" name="langd2" max="100" placeholder="Längd" required="required">
             <input type="text" id="Film" name="genre3" max="100" placeholder="Genre" required="required">
             <input type="number" id="Film" name="pid3" max="100" placeholder="Person ID" required="required">
             <input type="submit">
